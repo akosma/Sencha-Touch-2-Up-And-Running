90559c9ba2e10fed92b60a2396b2f52c44d09307
Adrian Kosmaczewski <adrian@akosma.com>
2012-08-11 19:09:43 +0200

    Added icons to the main sample code app

--- app.js
+++ app.js
@@ -43,6 +43,23 @@ var store = Ext.create('Ext.data.Store', {
 
 Ext.application({
     name: 'SenchaTouch2UpAndRunning',
+    icon: {
+        57: 'img/Icon.png',
+        72: 'img/Icon-iPad.png',
+        114: 'img/Icon@2x.png',     // Retina iPhone
+        144: 'img/Icon-iPad@2x.png' // Retina iPad
+    },
+
+    // http://www.sencha.com/forum/showthread.php?199382-phoneStartupScreen-doesn-t-work
+    startupImage: {
+        '320x460': 'img/Default.png',
+        '640x920': 'img/Default@2x.png', // Retina iPhone
+        '768x1004': 'img/Default-Portrait.png',
+        '748x1024': 'img/Default-Landscape.png',
+        '1536x2008': 'img/Default-Portrait@2x.png', // Retina iPad, Portrait
+        '1496x2048': 'img/Default-Landscape@2x.png' // Retina iPad, Landscape
+    },
+
     launch: function () {
         Ext.Viewport.add({
             xtype: 'list',
