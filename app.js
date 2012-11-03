3067bddbd7869c8b3d3f5b01e70e70a751300787
Adrian Kosmaczewski <adrian@akosma.com>
2012-11-03 12:46:57 +0100

    Added more chapters to the index bar of the main app

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ Ext.application({
             store: store,
             grouped: true,
             indexBar: {
-                letters: ['2', '3', '4', '5', '6', '7', '8', '9', '10']
+                letters: ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
             },
             items: [{
                 xtype: 'titlebar',
