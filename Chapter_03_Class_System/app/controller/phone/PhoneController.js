cfe03de1a08342a426c14ec0c69683758021d303
Adrian Kosmaczewski <adrian@akosma.com>
2012-11-05 17:58:14 +0100

    Better management of memory

--- Chapter_03_Class_System/app/controller/phone/PhoneController.js
+++ Chapter_03_Class_System/app/controller/phone/PhoneController.js
@@ -12,10 +12,14 @@ Ext.define('Chapter3ClassSystem.controller.phone.PhoneController', {
     },
 
     indexViewItemTap: function(nestedlist, list, index, target, record, e, eOpts) {
-        var screen = record.get('screen');
-        nestedlist.setDetailCard({
-            xtype: screen
-        });
+        var screenName = record.get('screen');
+        if (!this.screens) {
+            this.screens = {};
+        }
+        if (!this.screens[screenName]) {
+            this.screens[screenName] = Ext.createByAlias('widget.' + screenName);
+        }
+        nestedlist.setDetailCard(this.screens[screenName]);
     }
 });
 
