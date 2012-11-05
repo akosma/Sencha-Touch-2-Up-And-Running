be414a959d8c806173025673b0e02c8ff11c8a05
Adrian Kosmaczewski <adrian@akosma.com>
2012-11-05 12:51:31 +0100

    Finished the array demos

--- Chapter_03_Class_System/app/view/ArrayDemo.js
+++ Chapter_03_Class_System/app/view/ArrayDemo.js
@@ -7,12 +7,11 @@ Ext.define('Chapter3ClassSystem.view.ArrayDemo', {
     },
 
     getFunctions: function () {
-        return ['min'];
-        //return ['clean', 'contains', 'difference', 'erase', 
-            //'every', 'filter', 'flatten', 'from', 'include', 'indexOf',
-            //'insert', 'intersect', 'map', 'max', 'mean', 'merge', 'min',
-            //'pluck', 'remove', 'replace', 'slice', 'some', 'sort', 'splice',
-            //'sum', 'toArray', 'union', 'unique'];
+        return ['clean', 'contains', 'difference', 'erase', 
+            'every', 'filter', 'flatten', 'from', 'include', 'indexOf',
+            'insert', 'intersect', 'map', 'max', 'mean', 'merge', 'min',
+            'pluck', 'remove', 'replace', 'slice', 'some', 'sort', 'splice',
+            'sum', 'toArray', 'union', 'unique'];
     },
 
     cleanDemo: function () {
@@ -69,6 +68,7 @@ Ext.define('Chapter3ClassSystem.view.ArrayDemo', {
             // In this case, however, as 'whatever' is not contained in the 
             // sampleArray, the function always returns true, and 'result'
             // contains true as well.
+            // This function is very similar to 'some()'
             return (item !== 'whatever');
         }, this);
         console.log('Result of `every()`: ' + result);
@@ -157,40 +157,166 @@ Ext.define('Chapter3ClassSystem.view.ArrayDemo', {
         this.showTitleText('Maximum value', max);
     },
 
-    meanDemo: function () {},
+    meanDemo: function () {
+        var sampleArray = [1234, 456, -234, 654, 0];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var mean = Ext.Array.mean(sampleArray);
+        console.log('Mean value: ' + mean);
+        this.showTitleText('Mean value', mean);
+    },
 
-    mergeDemo: function () {},
+    mergeDemo: function () {
+        var sampleArray = [1234, 456, true, 'test'];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var anotherArray = [true, false, 'test', 'whatever', 456];
+        console.log('Another Array');
+        console.dir(anotherArray);
+        var merge = Ext.Array.merge(anotherArray, sampleArray);
+        this.showArray('Merged Array', merge);
+    },
 
     minDemo: function () {
         var sampleArray = [1234, 456, -234, 654, 0, 'whatever', "BOOM", true, 'test'];
         console.log('Original Array');
         console.dir(sampleArray);
-        var max = Ext.Array.min(sampleArray);
-        console.log('Minimum value: ' + max);
-        this.showTitleText('Minimum value', max);
+        var min = Ext.Array.min(sampleArray);
+        console.log('Minimum value: ' + min);
+        this.showTitleText('Minimum value', min);
     },
 
-    pluckDemo: function () {},
+    pluckDemo: function () {
+        var sampleArray = [{
+            key1: 'value1',
+            key2: 'value2'
+        }, {
+            key1: 'value3',
+            key2: 'value4'
+        }, {
+            key1: 'value5', 
+            key2: 'value6'
+        }, {
+            key1: 'value7',
+            key2: 'value8'
+        }, {
+            key1: 'value9',
+            key2: 'value0'
+        }];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var plucked = Ext.Array.pluck(sampleArray, 'key1');
+        this.showArray('Plucked Array', plucked);
+    },
 
-    removeDemo: function () {},
+    removeDemo: function () {
+        var sampleArray = [1234, 456, -234, 654, 0, 'whatever', "BOOM", true, 'test'];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        Ext.Array.remove(sampleArray, 'whatever');
+        Ext.Array.remove(sampleArray, -234);
+        Ext.Array.remove(sampleArray, true);
+        Ext.Array.remove(sampleArray, 'non existent value');
+        this.showArray('Array after removing items', sampleArray);
+    },
 
-    replaceDemo: function () {},
+    replaceDemo: function () {
+        var sampleArray = [1234, 456, -234, 654, 0, 'whatever', "BOOM", true, 'test'];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var replacements = ['boom', 'boom again'];
+        console.log('Replacements');
+        console.dir(replacements);
+        Ext.Array.replace(sampleArray, 4, 2, replacements);
+        this.showArray('Array after replacements', sampleArray);
+    },
 
-    sliceDemo: function () {},
+    sliceDemo: function () {
+        var sampleArray = [1234, 456, -234, 654, 0, 'whatever', "BOOM", true, 'test'];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var slice = Ext.Array.slice(sampleArray, 4, 7);
+        this.showArray('Sliced array', slice);
+    },
 
-    someDemo: function () {},
+    someDemo: function () {
+        var sampleArray = [1234, true, 'asdfadf', null, 'another string', '', 0];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var result = Ext.Array.some(sampleArray, function (item) {
+            // If here the inner function returns "true" at any point, the 
+            // loop ends and the value in "result" will be "true".
+            // In this case, however, as 'another string' is contained in the 
+            // sampleArray, the function returns true at some point, and 'result'
+            // contains true as well.
+            // This function is very similar to 'every()'
+            return (item === 'another string');
+        }, this);
+        console.log('Result of `some()`: ' + result);
+        this.showTitleText('Ext.some()', result);
+    },
 
-    sortDemo: function () {},
+    sortDemo: function () {
+        var sampleArray = [1234, true, 'ASDFADF', -76, '', null, 'another string', '', 0];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var sorted = Ext.Array.sort(sampleArray, function (item1, item2) {
+            if (item1 === item2) {
+                return 0;
+            }
+            return (item1 < item2) ? -1 : 1;
+        });
+        this.showArray('Sorted array', sorted);
+    },
 
-    spliceDemo: function () {},
+    spliceDemo: function () {
+        var sampleArray = [1234, 456, -234, 654, 0, 'whatever', "BOOM", true, 'test'];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        // Similar to 'replace()', but the signature here 
+        // is exactly the similar as in the standard JavaScript function
+        // of the same name; the replacements are passed as variadic values,
+        // after the third parameter.
+        Ext.Array.splice(sampleArray, 4, 2, 'boom', 'boom again');
+        this.showArray('Array after splice', sampleArray);
+    },
 
-    sumDemo: function () {},
+    sumDemo: function () {
+        var sampleArray = [1234, 456, -234, 654, 0, 'whatever'];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var sum = Ext.Array.sum(sampleArray);
+        console.log('Sum: ' + sum);
+        this.showTitleText('Sum', sum);
+    },
 
-    toArrayDemo: function () {},
+    toArrayDemo: function () {
+        var iterable = {
+            0: 'value0',
+            1: 'value1',
+            2: 'value2',
+            3: 'value3',
+            4: 'value4',
+            length: 5
+        };
+        console.log('Original iterable object');
+        console.dir(iterable);
+        var array = Ext.Array.toArray(iterable);
+        this.showArray('Array created from iterable', array);
+    },
 
-    unionDemo: function () {},
+    unionDemo: function () {
+        // 'union()' is an alias for 'merge()'
+        this.mergeDemo();
+    },
 
-    uniqueDemo: function () {},
+    uniqueDemo: function () {
+        var sampleArray = [1234, 456, 'whatever', 'whatever', 456, 'whatever'];
+        console.log('Original Array');
+        console.dir(sampleArray);
+        var unique = Ext.Array.unique(sampleArray);
+        this.showArray('Unique values', unique);
+    },
 
     showArray: function (title, array) {
         var text = [];
