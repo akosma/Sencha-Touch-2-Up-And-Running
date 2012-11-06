493a4d025b5cbae3aad7853008b17afb8d71aca2
Adrian Kosmaczewski <adrian@akosma.com>
2012-11-06 09:58:43 +0100

    Stub of the Ext functions demo

--- Chapter_03_Class_System/app/view/ExtObjDemo.js
+++ Chapter_03_Class_System/app/view/ExtObjDemo.js
@@ -3,6 +3,123 @@ Ext.define('Chapter3ClassSystem.view.ExtObjDemo', {
     xtype: 'extdemo',
     config: {
         html: 'Ext Demo'
+    },
+
+    getPrefix: function () {
+        return 'Ext';
+    },
+
+    getInstructions: function () {
+        return '';
+    },
+
+    getFunctions: function () {
+        return [ 'decode', 'encode', 'getBody', 'getClass', 'getClassName',
+            'getCmp', 'getDisplayName', 'getDoc', 'getHead', 'getStore',
+            'id', 'isArray', 'isBoolean', 'isDate', 'isDefined', 'isElement',
+            'isEmpty', 'isFunction', 'isIterable', 'isNumber', 'isNumeric',
+            'isObject', 'isPrimitive', 'isString', 'isTextNode', 'iterate',
+            'merge', 'namespace', 'ns', 'repaint', 'typeOf', 'valueFrom', 'widget' ];
+    },
+
+    decodeDemo: function () {
+    },
+
+    encodeDemo: function () {
+    },
+
+    getBodyDemo: function () {
+    },
+
+    getClassDemo: function () {
+    },
+
+    getClassNameDemo: function () {
+    },
+
+    getCmpDemo: function () {
+    },
+
+    getDisplayNameDemo: function () {
+    },
+
+    getDocDemo: function () {
+    },
+
+    getHeadDemo: function () {
+    },
+
+    getStoreDemo: function () {
+    },
+
+    idDemo: function () {
+    },
+
+    isArrayDemo: function () {
+    },
+
+    isBooleanDemo: function () {
+    },
+
+    isDateDemo: function () {
+    },
+
+    isDefinedDemo: function () {
+    },
+
+    isElementDemo: function () {
+    },
+
+    isEmptyDemo: function () {
+    },
+
+    isFunctionDemo: function () {
+    },
+
+    isIterableDemo: function () {
+    },
+
+    isNumberDemo: function () {
+    },
+
+    isNumericDemo: function () {
+    },
+
+    isObjectDemo: function () {
+    },
+
+    isPrimitiveDemo: function () {
+    },
+
+    isStringDemo: function () {
+    },
+
+    isTextNodeDemo: function () {
+    },
+
+    iterateDemo: function () {
+    },
+
+    mergeDemo: function () {
+    },
+
+    namespaceDemo: function () {
+    },
+
+    nsDemo: function () {
+    },
+
+    repaintDemo: function () {
+    },
+
+    typeOfDemo: function () {
+    },
+
+    valueFromDemo: function () {
+    },
+
+    widgetDemo: function () {
     }
+
 });
 
