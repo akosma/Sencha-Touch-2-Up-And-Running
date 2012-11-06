0f0eadd41fe4acbe82567549255eafe1be85b04f
Adrian Kosmaczewski <adrian@akosma.com>
2012-11-06 14:02:36 +0100

    Started to implement the function demo

--- Chapter_03_Class_System/app/view/FunctionDemo.js
+++ Chapter_03_Class_System/app/view/FunctionDemo.js
@@ -1,8 +1,96 @@
 Ext.define('Chapter3ClassSystem.view.FunctionDemo', {
     extend: 'Chapter3ClassSystem.view.DemoPanel',
     xtype: 'functiondemo',
-    config: {
-        html: 'Function Demo'
+
+    getPrefix: function () {
+        return 'Ext.Function';
+    },
+
+    getFunctions: function () {
+        return [ 'createDelayed', 'createInterceptor', 'createSequence', 'createThrottled',
+            'defer', 'flexSetter', 'pass' ];
+    },
+
+    createDelayedDemo: function () {
+        var fn = function (value) {
+            console.log('this is fn, this is the value: "' + value + '"');
+            this.showTitleText('This has been delayed', 'for about 1500 ms, and "' + value + '" was passed as parameter');
+        };
+
+        var delayed = Ext.Function.createDelayed(fn, 1500, this, [ 345 ]);
+
+        delayed();
+    },
+
+    createInterceptorDemo: function () {
+        var self = this;
+
+        var originalFn = function (value) {
+            console.log('this is the original function, this is the value: "' + value + '"');
+            self.showTitleText('This has been intercepted until "' + value + '" was passed as parameter');
+        };
+
+        var newFn = function (value) {
+            console.log('inside newFn, value is "' + value + '"');
+            return (value === 234);
+        };
+
+        var interceptor = Ext.Function.createInterceptor(originalFn, newFn);
+
+        interceptor(0);
+        interceptor(666);
+        interceptor(234);
+    },
+
+    createSequenceDemo: function () {
+        var first = function (value) {
+            console.log('inside the first() function, value = "' + value + '"');
+            return 'first';
+        };
+
+        var second = function (value) {
+            console.log('inside the second() function, value = "' + value + '"');
+            return 'second';
+        };
+
+        var third = Ext.Function.createSequence(first, second);
+
+        var value = third('boom');
+        console.log('third() returns "' + value + '"');
+
+        this.showTitleText('createSequence() demo', 'third() returns "' + value + '"');
+    },
+
+    createThrottledDemo: function () {
+        var fn = function () {
+            console.log('This is being throttled');
+            this.showTitleText('createThrottle() demo', 'No matter how many times we call it, the throttled function only executes once every 4 seconds.');
+        };
+
+        var throttled = Ext.Function.createThrottled(fn, 4000, this);
+
+        // No matter how many times we try, this will only be executed
+        // once every 4 seconds.
+        throttled();
+        throttled();
+        throttled();
+        throttled();
+        throttled();
+        throttled();
+        throttled();
+    },
+
+    deferDemo: function () {
+        Ext.Function.defer(function (arg) {
+            console.log('This function has been deferred 1500 ms, arg = "' + arg + '"');
+            this.showTitleText('defer() demo', 'This function has been deferred 1500 ms, arg = "' + arg + '"');
+        }, 1500, this, [ 'argument1' ]);
+    },
+
+    flexSetterDemo: function () {
+    },
+
+    passDemo: function () {
     }
 });
 
