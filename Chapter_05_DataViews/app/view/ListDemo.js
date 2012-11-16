08a80807b177b1ecbe5be352e43c36a68a02160d
Adrian Kosmaczewski <adrian@akosma.com>
2012-11-16 13:28:36 +0100

    Very simple list demo :)

--- Chapter_05_DataViews/app/view/ListDemo.js
+++ Chapter_05_DataViews/app/view/ListDemo.js
@@ -2,7 +2,8 @@ Ext.define('Chapter5DataViews.view.ListDemo', {
     extend: 'Ext.Panel',
     xtype: 'listdemo',
     config: {
-    
+        title: 'List Demo',
+        html: 'The list component is used for the main menu of this sample :)'
     }
 });
 
