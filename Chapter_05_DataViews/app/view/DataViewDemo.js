9ebe38a6ca29200709836a310a4c0e9010226d47
Adrian Kosmaczewski <adrian@akosma.com>
2012-11-16 14:47:22 +0100

    Added event handlers for the dataview component

--- Chapter_05_DataViews/app/view/DataViewDemo.js
+++ Chapter_05_DataViews/app/view/DataViewDemo.js
@@ -5,7 +5,25 @@ Ext.define('Chapter5DataViews.view.DataViewDemo', {
         title: 'DataView Demo',
         id: 'demoDataView',
         xtype: 'dataview',
-        itemTpl: '<div style="" class="tweetdiv"><div class="avatar"><img src="{profile_image_url}" /></div><div class="text"><p class="username">{from_user}</p><p class="tweet">{text}</p><p class="date">{[Ext.Date.format(values.created_at, "d.m.Y, H:i")]}</p></div></div>',
+        listeners:  {
+            itemsingletap: function (dataview, index, target, record, e, eOpts) {
+                var id = record.get('id');
+                var author = record.get('from_user');
+                var message = Ext.String.format('Tweet {0}<br>from {1}', id, author);
+                Ext.Msg.alert('Information', message);
+            },
+            itemswipe: function (dataview, index, target, record, e, eOpts) {
+                var id = record.get('id');
+                var message = Ext.String.format('Swiping! {0}', id);
+                Ext.Msg.alert(message);
+            },
+            itemdoubletap: function (dataview, index, target, record, e, eOpts) {
+                var name = record.get('from_user_name');
+                var message = Ext.String.format('Double tap! {0}', name);
+                Ext.Msg.alert(message);
+            }
+        },
+        itemTpl: '<div class="tweetdiv"><div class="avatar"><img src="{profile_image_url}" /></div><div class="text"><p class="username">{from_user_name}</p><p class="tweet">{text}</p><p class="date">{[Ext.Date.format(values.created_at, "d.m.Y, H:i")]}</p></div></div>',
         store: {
             autoLoad: true,
             fields: [{
