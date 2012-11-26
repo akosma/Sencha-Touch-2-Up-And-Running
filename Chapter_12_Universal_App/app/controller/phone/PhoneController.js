d56d5b5fc1943251a666ce2fc699863e7a579481
Adrian Kosmaczewski <adrian@akosma.com>
2012-11-26 22:38:45 +0100

    Trying to solve crashing issues

--- Chapter_12_Universal_App/app/controller/phone/PhoneController.js
+++ Chapter_12_Universal_App/app/controller/phone/PhoneController.js
@@ -7,7 +7,7 @@ Ext.define('Chapter12Universal.controller.phone.PhoneController', {
         },
         control: {
             listView: {
-                itemtap: 'listViewItemTap' 
+                itemsingletap: 'listViewItemTap' 
             }
         }
     },
@@ -15,16 +15,10 @@ Ext.define('Chapter12Universal.controller.phone.PhoneController', {
     listViewItemTap: function(list, index, target, record, e, eOpts) {
         var screenName = record.get('screen');
         var text = record.get('text');
-        if (!this.screens) {
-            this.screens = {};
-        }
-        if (!this.screens[screenName]) {
-            this.screens[screenName] = Ext.widget('panel', {
-                html: text,
-                title: text
-            });
-        }
-        this.getIndexView().push(this.screens[screenName]);
+        this.getIndexView().push(Ext.widget('panel', {
+            html: text,
+            title: text
+        }));
     }
 });
 
