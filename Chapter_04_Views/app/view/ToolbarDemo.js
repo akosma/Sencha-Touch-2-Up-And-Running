2d018ed5954cd0cd87f3236744a011100e75d420
Adrian Kosmaczewski <adrian@akosma.com>
2012-12-19 11:42:47 +0100

    Added a demo for toolbars

--- Chapter_04_Views/app/view/ToolbarDemo.js
+++ Chapter_04_Views/app/view/ToolbarDemo.js
@@ -2,7 +2,47 @@ Ext.define('Chapter4Views.view.ToolbarDemo', {
     extend: 'Ext.Panel',
     xtype: 'toolbardemo',
     config: {
-        html: 'ToolbarDemo'
+        items: [{
+            xtype: 'label',
+            html: 'The toolbar is very similar to the title bar. They can contain "spacer" objects, which as the name implies create proportional, flexible spaces between groups of buttons.',
+            centered: true,
+            margin: 20
+        }, {
+            xtype: 'toolbar',
+            docked: 'top',
+            items: [{
+                iconCls: 'refresh',
+                iconMask: true
+            }, {
+                iconCls: 'home',
+                iconMask: true
+            }, {
+                iconCls: 'action',
+                iconMask: true
+            }, {
+                xtype: 'spacer'
+            }, {
+                iconCls: 'user',
+                iconMask: true,
+                align: 'right',
+                text: 'Profile'
+            }, {
+                iconCls: 'download',
+                iconMask: true
+            }]
+        }, {
+            xtype: 'toolbar',
+            docked: 'bottom',
+            items: [{
+                iconCls: 'favorites',
+                iconMask: true
+            }, {
+                xtype: 'spacer'
+            }, {
+                iconCls: 'search',
+                iconMask: true
+            }]
+        }]
     }
 });
 
