900cf632137b8bff360a36253d908fd56da585d3
Adrian Kosmaczewski <adrian@akosma.com>
2012-12-19 12:38:15 +0100

    Fixed icon paths and app titles for several samples

--- Chapter_05_Data/app.js
+++ Chapter_05_Data/app.js
@@ -14,21 +14,21 @@ Ext.application({
     profiles: ['Phone', 'Tablet'],
 
     icon: {
-        57: '../img/Icon.png',
-        72: '../img/Icon-iPad.png',
-        114: '../img/Icon@2x.png',     // Retina iPhone
-        144: '../img/Icon-iPad@2x.png' // Retina iPad
+        57: '../img/icons/Icon.png',
+        72: '../img/icons/Icon-iPad.png',
+        114: '../img/icons/Icon@2x.png',     // Retina iPhone
+        144: '../img/icons/Icon-iPad@2x.png' // Retina iPad
     },
 
     // http://www.sencha.com/forum/showthread.php?199382-phoneStartupScreen-doesn-t-work
     startupImage: {
-        '320x460': '../img/Default.png',
-        '640x920': '../img/Default@2x.png', // Retina iPhone
-        '640x1096': '../img/Default-568@2x.png', // Retina 4-inch iPhone
-        '768x1004': '../img/Default-Portrait.png',
-        '748x1024': '../img/Default-Landscape.png',
-        '1536x2008': '../img/Default-Portrait@2x.png', // Retina iPad, Portrait
-        '1496x2048': '../img/Default-Landscape@2x.png' // Retina iPad, Landscape
+        '320x460': '../img/loading/Default.png',
+        '640x920': '../img/loading/Default@2x.png', // Retina iPhone
+        '640x1096': '../img/loading/Default-568@2x.png', // Retina 4-inch iPhone
+        '768x1004': '../img/loading/Default-Portrait.png',
+        '748x1024': '../img/loading/Default-Landscape.png',
+        '1536x2008': '../img/loading/Default-Portrait@2x.png', // Retina iPad, Portrait
+        '1496x2048': '../img/loading/Default-Landscape@2x.png' // Retina iPad, Landscape
     },
 
     models: ['Person'],
