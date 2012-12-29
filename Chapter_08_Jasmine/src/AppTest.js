56858655e1dbc2ebdd0aaeb67933365cb3cf47a4
Adrian Kosmaczewski <adrian@akosma.com>
2012-12-29 18:23:40 +0100

    Removed comment

--- Chapter_08_Jasmine/src/AppTest.js
+++ Chapter_08_Jasmine/src/AppTest.js
@@ -14,7 +14,6 @@ Ext.onReady(function() {
         models: [ 'Task' ],
         
         launch: function() {
-            //include the tests in the test.html head
             jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
             jasmine.getEnv().execute();
         }
