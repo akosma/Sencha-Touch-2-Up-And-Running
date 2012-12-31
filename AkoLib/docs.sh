50c6615ebb3128d379f6084c97716dd9ba93e8e1
Adrian Kosmaczewski <adrian@akosma.com>
2012-12-31 07:42:08 +0100

    Changed doc gen script

--- AkoLib/docs.sh
+++ AkoLib/docs.sh
@@ -2,5 +2,5 @@
 
 # This script requires the installation of jsduck
 # using 'gem install jsduck'
-jsduck AkoLib/view/SplitView.js --external Ext.Component --external Ext.Container --output docs
+jsduck view/SplitView.js --external Ext.Component --external Ext.Container --output docs
 
