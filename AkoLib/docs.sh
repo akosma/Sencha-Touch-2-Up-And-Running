18dffe1d2b9109c9c12eb718ed5e50ea316f36ec
Adrian Kosmaczewski <adrian@akosma.com>
2013-01-03 15:56:39 +0100

    Changed the documentation generator script

--- AkoLib/docs.sh
+++ AkoLib/docs.sh
@@ -2,5 +2,5 @@
 
 # This script requires the installation of jsduck
 # using 'gem install jsduck'
-jsduck view/SplitView.js --external Ext.Component --external Ext.Container --output docs
+jsduck view/SplitView.js --external Ext.Component --external Ext.Container --output ../Chapter_08_JSDuck
 
